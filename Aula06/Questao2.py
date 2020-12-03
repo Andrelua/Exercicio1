8b00f246c4f318d0839822e91dcf9f97924eac80
André Luan <Andreluan176@gmail.com>
2020-12-03 18:04:58 -0300

    Questão 2

--- Aula06/Questao2.py
+++ Aula06/Questao2.py
@@ -4,3 +4,26 @@ através da função criada:
 a. De 1 até 10, de 1 em 1
 b. De 10 até 0, de 2 em 2
 c. Um contagem personalizada"""
+
+
+def contador(inicio, fim, passo=1):
+    if passo >= 1:
+        print(inicio)
+        for i in range(inicio, fim):
+            inicio = inicio + passo
+            print(inicio)
+            if (inicio <= fim):
+                break
+    else:
+        print(inicio)
+        for i in range(fim, inicio):
+            inicio = inicio + passo
+            print(inicio)
+            if inicio > 0:
+                break
+                
+
+
+contador(1,10) # De 1 até 10, de 1 em 1
+contador(10, 1, -2) # De 10 até 0, de 2 em 2
+contador(1, 100, 10) # 
