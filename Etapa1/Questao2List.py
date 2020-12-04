9b3c306bb2043fbc33944fa115906fc158740fb6
André Luan <Andreluan176@gmail.com>
2020-12-04 20:47:43 -0300

    Resoluçao questão 2

--- Etapa1/Questao2List.py
+++ Etapa1/Questao2List.py
@@ -1,2 +1,18 @@
-# Leia um vetor de 10 posições e verifique se o número digitado ja foi armazenado, se sim, leia novamente. No final mostre todos os nnúmeros. 
+# Leia um vetor de 10 posições e verifique se o número digitado ja foi armazenado, se sim, leia novamente. No final mostre todos os números. 
 # Use lista.
+
+num = []
+for i in range(0, 10):
+    while True:
+        if len(num) == 10:
+            break
+        num2 = int(input('Informe o número: '))
+        if num2 not in num:
+            num.append(num2)
+        else:
+            print('Este número ja está armazenado.')
+            num2 = int(input('Armazene outro número: '))
+        
+                
+
+print(num)
