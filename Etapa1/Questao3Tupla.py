be3109bf3461043ddaa3205133bfd0501b3a3cf9
André Luan <Andreluan176@gmail.com>
2020-12-04 20:55:56 -0300

    Resolução da questão 3

--- Etapa1/Questao3Tupla.py
+++ Etapa1/Questao3Tupla.py
@@ -1,1 +1,8 @@
 # Crie dois vetores A e B, preencha cada um com 5 valores e por último calcule: C = A - B. Utilize tupla.
+
+a = (19, 7, 67, 89, 56)
+b = (8, 2, 8, 99, 45)
+
+for i in range(0, 5):
+    print(f'C na {i+1}º posição = {a[i] - b[i]}')
+    
