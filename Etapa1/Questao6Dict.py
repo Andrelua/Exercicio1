67244d8fba4202bc2687865a0f3aa3523da6bc23
André Luan <Andreluan176@gmail.com>
2020-12-04 21:35:40 -0300

    Resoluçao da questão 6

--- Etapa1/Questao6Dict.py
+++ Etapa1/Questao6Dict.py
@@ -8,3 +8,8 @@
 # Egito = 14°
 # Roma = 13°
 # Utilize dicionário
+
+paises = {'Paris': 51, 'Recife': 26, 'Hong Kong': 16, 'Nova Iorque': 9, 'Barcelona': 15, 'Tóquio': 11, 'Egito': 14, 'Roma': 13}
+
+for chave, valor in paises.items():
+    print(f'{chave} com {(valor * 9/5) + 32 } F°')
