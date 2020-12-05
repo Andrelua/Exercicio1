19779c4dcc8866d09e4c6488aefda0b60e83931d
André Luan <Andreluan176@gmail.com>
2020-12-04 21:31:29 -0300

    Resolução questão 5

--- Etapa1/Questao5Dict.py
+++ Etapa1/Questao5Dict.py
@@ -1,5 +1,22 @@
-# Com os meses do ano e seus respectivos dias faça:     
-# Ordene do primeiro ao último;
-# Ordene do último ao primeiro;
+# Com os meses do ano e seus respectivos dias faça:
 # Mostre os meses com 31 dias.
+# Mostre os meses com 30 dias.
 # Utilize dicionário.
+
+
+meses = {'Jan': 31, 'Fev': 29, 'Mar': 31, 'Abr': 30, 'Mai': 31, 'Jun': 30, 'Jul': 31, 'Ago': 31, 'Set': 30, 'Out': 31, 'Nov': 30, 'Dez': 31}
+
+print('Meses com 31 dias.')
+for chave, valor in meses.items():
+    if valor == 31:
+        print(f'{chave} {valor} dias')
+
+print('Meses com 30 dias.')
+for chave, valor in meses.items():
+    if valor == 30:
+        print(f'{chave} {valor} dias')
+
+print('Meses com menos de 30 dias.')
+for chave, valor in meses.items():
+    if valor < 30:
+        print(f'{chave} {valor} dias')
