7c18a1d3e9b94569e80da4a35e7271a34c4abb68
André Luan <Andreluan176@gmail.com>
2020-12-04 21:10:42 -0300

    Resoluçao questão 4

--- Etapa1/Questao4Tupla.py
+++ Etapa1/Questao4Tupla.py
@@ -1,1 +1,16 @@
-# Preencha um vetor com 50 posições com a seguinte fórmula: (i + 5 * i) % ( i + 1), sendo i a posição no vetor. Imprima os valores. Utilize tupla.
+#Dado determinados times, mostre os 5 primeiros, em ordem alfabética e os últimos 4. Utilize tupla.
+
+times = ('Palmeiras', 'Sport', 'Náutico', 'Santana Cruz', 'São Paulo', 'Gremio', 'Salgueiro', 'ABC', 'BotaFogo', 'Atletico-MG')
+
+print("# 5 primeiros")
+for i in range(0, 5):
+    print(times[i])
+
+print('# Ordem alfabética')
+print(sorted(times))
+
+print('# Os últimos 4')
+a = -4
+for i in range(0, 4):
+    print(times[a])
+    a = a + 1
