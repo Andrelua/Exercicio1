e2b68e42570529fec73c1b46994a795e46c7f72d
André Luan <Andreluan176@gmail.com>
2020-12-09 19:05:05 -0300

    Atividades, questão 1 Lista

--- Etapa2/exercicio14/Exercicio_Etapa-1_Lista_Q1.py
+++ Etapa2/exercicio14/Exercicio_Etapa-1_Lista_Q1.py
@@ -3,3 +3,31 @@ esta pesquisando o preço em várias lojas. Faça
 um programa usando lista onde Tiago ira digitar o nome da loja e o 
 valor do fone de ouvido, no final.
 Exiba qual loja tem o menor e o maior preço.'''
+
+listaLojas = []
+while True:
+    lista02 = []
+    for i in range(0, 1):
+        lista02.append(input("Informe o nome da loja: "))
+        lista02.append(int(input("Informe o valor do fone nessa loja: ")))
+    listaLojas.append(lista02)
+    exit = input("Deseja encerrar (Y/N): ")
+    if exit == "Y":
+        break
+
+maior = 0
+nomeMaior = 0
+for i in range(0, len(listaLojas)):
+    if listaLojas[i][1] > maior:
+        maior = listaLojas[i][1]
+        nomeMaior = listaLojas[i][0]
+
+menor = 99999999999
+nomeMenor = 0
+for i in range(0, len(listaLojas)):
+    if listaLojas[i][1] < menor:
+        menor = listaLojas[i][1]
+        nomeMenor = listaLojas[i][0]
+
+print(f"O maior preço foi da loja {nomeMaior} e foi de {maior}. ")
+print(f"O menor preço foi da loja {nomeMenor} e foi de {menor}. ")
