584ca1b9b97e4efcc6c917774d7209f20636ea0e
André Luan <Andreluan176@gmail.com>
2020-12-09 18:15:24 -0300

    Atividades, questão 2 Tupla

--- Etapa2/exercicio14/Exercicio_Etapa-1_Tupla_Q2.py
+++ Etapa2/exercicio14/Exercicio_Etapa-1_Tupla_Q2.py
@@ -6,3 +6,18 @@ faça um sorteio utilizando tupla para decidir o filme,
 porém se eles decidirem assistir o filme depois das 18:00 horas
 só poderão assistir os filmes de terror que são 
 (Corra ou Um Lugar Silencioso).'''
+
+import random as rd
+
+filmes = ("O Poço", "Sementes Podres", "Bacurau", "Corra", "Um Lugar Silencioso")
+filmes18 = ("Corra", "Um Lugar Silencioso")
+
+horario = float(input("Em que horário irão assistir o filme (EX: 18.30): "))
+
+i = 0
+if horario < 18:
+    i = rd.randint(0,4)
+    print(f"Vamos assitir o filme {filmes[i]}")
+else:
+    i = rd.randint(0,1)
+    print(f"Vamos assitir o filme {filmes18[i]}")
