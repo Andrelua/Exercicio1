a6cfef50f12f35e4ef7cdb271a02219b829ea775
André Luan <Andreluan176@gmail.com>
2020-12-09 18:39:22 -0300

    Atividades, questão 2 Lista

--- Etapa2/exercicio14/Exercicio_Etapa-1_Lista_Q2.py
+++ Etapa2/exercicio14/Exercicio_Etapa-1_Lista_Q2.py
@@ -2,3 +2,12 @@
 salario, ele deseja saber qual mês do ano ele econimizou
 o maior e o menor valor e quanto foi. Então faça um programa
  utilizando lista para resolver esse problema.'''
+salario = 1220
+economia = []
+for i in range(0, 12):
+    eco = int(input(f"Quantos porcentos ele economizou no {i+1}º mês ? "))
+    valoreco = salario * (eco / 100)
+    economia.append(valoreco)
+
+print(f"A maior economia foi de {max(economia)} e foi no {economia.index(max(economia))+1}º mês.")
+print(f"A menor economia foi de {min(economia)} e foi no {economia.index(min(economia))+1}º mês.")
