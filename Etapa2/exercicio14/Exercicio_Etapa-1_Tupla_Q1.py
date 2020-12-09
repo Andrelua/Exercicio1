ba3a2e8bee7affe686e0e0aed06daff70ea4e54d
André Luan <Andreluan176@gmail.com>
2020-12-09 18:28:49 -0300

    Atividades, questão 1 Tupla

--- Etapa2/exercicio14/Exercicio_Etapa-1_Tupla_Q1.py
+++ Etapa2/exercicio14/Exercicio_Etapa-1_Tupla_Q1.py
@@ -5,3 +5,16 @@ garrafas de água da geladeira, então faça um programa
 usando "tupla" para sortear quem ira ficar com cada função, 
 lembrando que a mesma pessoa não pode ficar com as duas
 funções e a filha(bia) não pode lavar a louça porque só tem 9 anos.'''
+import random as rd
+
+familia = ("Bia", "Antonio", "Maria", "Caio")
+
+i = rd.randint(0, 3)
+j = rd.randint(1,3)
+
+print(f"Quem vai lavar a louça é: {familia[i]}.")
+
+if familia[i] == familia[j]:
+    print(f"E quem também vai encher as garrafas de água da geladeira é {familia[j]}.")
+else:
+    print(f"E quem vai encher as garrafas de água da geladeira é {familia[j]}.")
