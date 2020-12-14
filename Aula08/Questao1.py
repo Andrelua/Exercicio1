64d34a7b6efe1e8b1de8476418ab349a79142fa1
André Luan <Andreluan176@gmail.com>
2020-12-13 22:14:37 -0300

    Questão 1 e 2

---
+++ Aula08/Questao1.py
@@ -0,0 +1,14 @@
+"""
+Faça um programa que ajude um jogador da Mega Sena a criar palpites. O
+programa vai perguntar quantos jogos serão gerados e vai sortear 6 números
+entre 1-60 para cada jogo, cadastrando tudo em uma lista composta.
+Exemplo:
+Quantos jogos você quer sortear? 2
+Jogo 1: [4,8,10,20,26,38]
+Jogo 2: [14, 20, 43, 35, 38,47]
+
+Obs: o número não pode se repetir no mesmo jogo. Mas, não tem problema se
+repetir em outro jogo.
+Além de exibir na tela o resultado do palpite.
+Você criar um arquivo com os mesmos resultados.
+"""
---
+++ Aula08/Questao2.py
@@ -0,0 +1,8 @@
+"""
+Faça um programa que permite cadastrar pessoas com os seguintes
+dados: nome e idade. Grave esses dados em um arquivo de txt.
+O sistema ainda terá opção de cadastrar um nova pessoa e de listar as
+pessoas que estão gravadas no arquivo.
+Ao cadastrar uma nova pessoa, não deve apagar os registros
+anteriores.
+"""
