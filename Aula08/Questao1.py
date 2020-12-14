e0382e0234252bed5bccf22469e12c1d16f0a6e7
André Luan <Andreluan176@gmail.com>
2020-12-14 19:25:06 -0300

    Questão 1

--- Aula08/Questao1.py
+++ Aula08/Questao1.py
@@ -12,7 +12,22 @@ repetir em outro jogo.
 Além de exibir na tela o resultado do palpite.
 Você criar um arquivo com os mesmos resultados.
 """
-from os import write
+from os import close
+import random as rd
 
 with open('Palpite.txt', 'w') as arquivo:
-    arquivo.write()
+    numeros_sorteados = []
+    num = int(input('Quantos jogos você quer sortear? '))
+    for y in range(1, num + 1):
+        while True:
+            numero = rd.randint(1, 60)
+            if numero not in numeros_sorteados:
+                numeros_sorteados.append(numero)
+
+            if len(numeros_sorteados) == 6:
+                break
+        print(f"Jogo {y}: {str(numeros_sorteados)}\n")
+        arquivo.write(f"Jogo {y}: {str(numeros_sorteados)}\n")
+        del(numeros_sorteados[0:6])
+    
+    arquivo.close()
