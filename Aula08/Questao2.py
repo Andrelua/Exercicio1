da840a41932bd722b0bf88883416234ff378fde7
André Luan <Andreluan176@gmail.com>
2020-12-14 21:42:23 -0300

    Questão 2, incompleta.

--- Aula08/Questao2.py
+++ Aula08/Questao2.py
@@ -6,3 +6,25 @@ pessoas que estão gravadas no arquivo.
 Ao cadastrar uma nova pessoa, não deve apagar os registros
 anteriores.
 """
+
+# Não consegui fazer
+
+with open('Cadastro.txt', 'a') as arquivo:
+    while True:
+        print("------- Cadastro -------")
+        resposta = int(input("Opção: \n1 - Cadastrar \n2 - Listar \n0 - Sair\n"))
+        
+        if resposta == 1:
+            nome = input("Nome: ")
+            idade = input("Idade: ")
+            arquivo.write(f"Nome: {nome} / Idade: {idade}.\n")
+        elif resposta == 2:
+            arquivo = open('Cadastro.txt', 'r')
+            for line in arquivo:
+                print(line)
+        else:
+            arquivo.close()
+            break
+        
+
+    
